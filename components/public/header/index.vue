03b4fa9da605c6cd27a3ea1678a967349cf39b13
AurorePaladin <aurorepaladin@gmail.com>
2018-11-18 23:45:19 +0800

    头部index

---
+++ components/public/header/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="m-header">
+    <el-row>
+      <topbar/>
+    </el-row>
+    <el-row>
+      <searchbar/>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import topbar from './topbar.vue'
+import searchbar from './searchbar.vue'
+export default {
+  components: {
+    topbar,
+    searchbar
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/css/public/header/index.scss';
+</style>
