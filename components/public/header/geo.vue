08536ff3a392786d11d4f2cd029a160bb5777c11
AurorePaladin <aurorepaladin@gmail.com>
2018-11-18 23:44:18 +0800

    头部地址栏

---
+++ components/public/header/geo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-loaction" />北京市
+    <nuxt-link 
+      class="changeCity" 
+      to="=&quot;/changeCity&quot;">切换城市</nuxt-link>
+    [香河 廊坊 天津]
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
