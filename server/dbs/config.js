661347fc3a4f731a5882e29fa4571a3f0c7409c6
AurorePaladin <aurorepaladin@gmail.com>
2018-12-03 23:46:22 +0800

    服务器设置

---
+++ server/dbs/config.js
@@ -0,0 +1,35 @@
+export default {
+  dbs: 'mangodb://127.0.0.1:27017/student',
+  redis: {
+    get host() {
+      return '127.0.0.1'
+    },
+    get port() {
+      return 6379
+    }
+  },
+  smtp: {
+    get host() {
+      return 'smtp.qq.com'
+    },
+    get user() {
+      return '3496051@qq.com'
+    },
+    get pass() {
+      return 'wshc bhct aiyr bgjj'
+    }
+  },
+  get code() {
+    return () => {
+      return Math.random()
+        .toString(16)
+        .slice(2, 6)
+        .toUpperCase()
+    }
+  },
+  get expire() {
+    return () => {
+      return new Date().getTime() + 60 * 60 * 1000
+    }
+  }
+}
