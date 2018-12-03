ba8c2d5b4aa58d02276900c058d8ab0254c95b61
AurorePaladin <aurorepaladin@gmail.com>
2018-12-03 23:47:29 +0800

    退出页面 逻辑修改

--- pages/exit.vue
+++ pages/exit.vue
@@ -5,10 +5,10 @@
 <script>
 export default {
   layout: 'blank',
-  middleware: async (ctx) => {
-    let {status,data}=await ctx.$axios.get('/users/exit')
-    if(status===200&&data&&data.code===0){
-      window.location.href='/'
+  middleware: async ctx => {
+    let { status, data } = await ctx.$axios.get('/users/exit')
+    if (status === 200 && data && data.code === 0) {
+      window.location.href = '/'
     }
   }
 }
