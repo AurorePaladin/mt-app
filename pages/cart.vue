7490488826f4aa9b8da1222389256e3be4b96519
AurorePaladin <aurorepaladin@gmail.com>
2018-12-05 23:33:37 +0800

    逻辑更新

--- pages/cart.vue
+++ pages/cart.vue
@@ -25,59 +25,69 @@
 <script>
 import List from '@/components/cart/list.vue'
 export default {
-  components:{
+  components: {
     List
   },
-  data(){
+  data() {
     return {
-      cart:[]
+      cart: []
     }
   },
-  computed:{
-    total(){
-      let total=0;
-      this.cart.forEach(item=>{
-        total+=item.price*item.count
+  computed: {
+    total() {
+      let total = 0
+      this.cart.forEach(item => {
+        total += item.price * item.count
       })
       return total
     }
   },
-  methods:{
-    submit: async function(){
-      let {status,data:{code,id}}=await this.$axios.post('/order/createOrder',{
-        count:this.cart[0].count,
-        price:this.cart[0].price,
-        id:this.cartNo
+  methods: {
+    submit: async function() {
+      let {
+        status,
+        data: { code, id }
+      } = await this.$axios.post('/order/createOrder', {
+        count: this.cart[0].count,
+        price: this.cart[0].price,
+        id: this.cartNo
       })
-      if(status==200&&code===0){
-        this.$alert(`恭喜您，已成功下单，订单号:${id}`,'下单成功',{
-          confirmButtonText:'确定',
-          callback:action=>{
-            location.href='/order'
+      if (status == 200 && code === 0) {
+        this.$alert(`恭喜您，已成功下单，订单号:${id}`, '下单成功', {
+          confirmButtonText: '确定',
+          callback: action => {
+            location.href = '/order'
           }
         })
       }
     }
   },
-  async asyncData(ctx){
-    let {status,data:{code,data:{name,price}}}=await ctx.$axios.post('/cart/getCart',{
-      id:ctx.query.id
+  async asyncData(ctx) {
+    let {
+      status,
+      data: {
+        code,
+        data: { name, price }
+      }
+    } = await ctx.$axios.post('/cart/getCart', {
+      id: ctx.query.id
     })
-    if(status===200&&code===0&&name){
+    if (status === 200 && code === 0 && name) {
       return {
-        cart:[{
-          name,
-          price,
-          count:1
-        }],
-        cartNo:ctx.query.id
+        cart: [
+          {
+            name,
+            price,
+            count: 1
+          }
+        ],
+        cartNo: ctx.query.id
       }
     }
   }
-
 }
 </script>
 
 <style lang="scss">
-  @import "@/assets/css/cart/index.scss";
+@import '@/assets/css/cart/index.scss';
 </style>
