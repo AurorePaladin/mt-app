4b6a4e26d6b2eaf39cebcb2d4c8d64244ff7dfc1
AurorePaladin <aurorepaladin@gmail.com>
2018-12-05 23:34:47 +0800

    逻辑更新 3

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,6 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
-
 export default {
   components: {
     Logo
@@ -22,7 +21,6 @@ export default {
   align-items: center;
   text-align: center;
 }
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -32,7 +30,6 @@ export default {
   color: #35495e;
   letter-spacing: 1px;
 }
-
 .subtitle {
   font-weight: 300;
   font-size: 42px;
@@ -40,7 +37,6 @@ export default {
   word-spacing: 5px;
   padding-bottom: 15px;
 }
-
 .links {
   padding-top: 15px;
 }
