e9a1276ca1d6780d174ea6abafcffa7d6ee45b98
AurorePaladin <aurorepaladin@gmail.com>
2018-12-07 23:35:58 +0800

    接口配置依赖更改

--- server/dbs/config.js
+++ server/dbs/config.js
@@ -17,19 +17,19 @@ export default {
     },
     get pass() {
       return 'wshc bhct aiyr bgjj'
-    }
-  },
-  get code() {
-    return () => {
-      return Math.random()
-        .toString(16)
-        .slice(2, 6)
-        .toUpperCase()
-    }
-  },
-  get expire() {
-    return () => {
-      return new Date().getTime() + 60 * 60 * 1000
+    },
+    get code() {
+      return () => {
+        return Math.random()
+          .toString(16)
+          .slice(2, 6)
+          .toUpperCase()
+      }
+    },
+    get expire() {
+      return () => {
+        return new Date().getTime() + 60 * 60 * 1000
+      }
     }
   }
 }
