eeb87f822b98803aeb6e4558d7cb09a2e6868b1e
AurorePaladin <aurorepaladin@gmail.com>
2018-12-08 23:53:10 +0800

    头部添加用户模块

--- components/public/header/user.vue
+++ components/public/header/user.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="m-user">
     <template v-if="user">
-      欢迎您， <span class="username">{{ user }}</span>
-      <nuxt-link to="/exit">退出</nuxt-link>
-    </template> 
+      欢迎您，<span class="username">{{ user }}</span>
+      [<nuxt-link to="/exit">退出</nuxt-link>]
+    </template>
     <template v-else>
       <nuxt-link
         to="/login"
         class="login">立即登录</nuxt-link>
       <nuxt-link
-        to="/register"
-        class="register">注册</nuxt-link>
-    </template>  
+        class="register"
+        to="/register">注册</nuxt-link>
+    </template>
   </div>
 </template>
 
@@ -21,6 +21,18 @@ export default {
     return {
       user: ''
     }
+  },
+  async mounted() {
+    const {
+      status,
+      data: { user }
+    } = await this.$axios.get('/users/getUser')
+    if (status === 200) {
+      this.user = user
+    }
   }
 }
 </script>
+
+<style lang="css">
+</style>
